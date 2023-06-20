297b6b1a5a94e52adfd682258b86862deabbc6c7
Juriy Gelvich <gelvihurij@gmail.com>
2023-06-21 04:40:51 +0700

    Adding Seminar07 folder

---
+++ PythonSeminar/SeminarTask/Seminar07/Stask01.py
@@ -0,0 +1,37 @@
+# Дан текстовый csv файл. Разделитель данных #.
+# Каждая строка файла представляет собой запись вида ФИО
+
+# Например:
+# Иванов#Иван#Иванович
+# Петров#Петр#Петрович
+# Соколов#Илья#Григорьевич
+
+# 1) Необходимо вывести эти данные на экран построчно в виде:
+# Иванов Иван Иванович
+# Петров Петр Петрович
+
+# 2) записать эти данные в список вида: [['Иванов', 'Иван', 'Иванович'], ['Петров', 'Петр', 'Петрович']...]
+
+# [*] Усложнение. Файл находится в поддиретории data текущей директории. Сформировать путь к нему с использованием
+
+# os.path и pathlib
+
+import os.path as path1
+# from os import getcwd 
+# print(getcwd())
+
+MAIN_DIR = path1.abspath(path1.dirname(__file__))
+file_name = path1.join(MAIN_DIR, "data1.txt")
+
+list_data = list()
+
+with open (file_name, "r", encoding="utf-8") as data:
+    for line in data:
+        # print(*line.strip().split("#"))
+        last_name, first_name, parent = line.strip().split("#")
+        print(last_name, first_name, parent)
+        list_data.append([last_name, first_name, parent])
+
+
+print(list_data)
+
---
+++ PythonSeminar/SeminarTask/Seminar07/Stask02.py
@@ -0,0 +1,39 @@
+# Продолжение предыдущей задачи
+# Данные в списке [['Иванов', 'Иван', 'Иванович'], ['Петров', 'Петр', 'Петрович']...]
+
+# необходимо преобразовать к виду:
+# Иванов И.И.
+# Петров П.П.
+
+# Полученные строки записать в новый файл. Файл должен находиться в поддиретории rezults.
+
+# [*] Усложнение. Данные необходимо записать в два разных файла:
+# В первый - в виде Иванов И.И.
+# Во второй - в виде Иванов-И-И
+
+# [*****] Усложнение. Вам известно, что (в перспективе) подобных спецификаций может быть много. Не две, а пять или десять
+# Как улучшить свой код в этом случае, сделать его легко расширяемым?
+
+import os.path as path1
+
+MAIN_DIR = path1.abspath(path1.dirname(__file__))
+file_name = path1.join(MAIN_DIR, "data1.txt")
+
+list_data = list()
+
+with open (file_name, "r", encoding="utf-8") as data:
+    for line in data:
+        # last_name, first_name, parent = line.strip().split("#")
+        list_data.append(line.strip().split("#"))
+
+print(list_data)
+
+file_name_two = path1.join(MAIN_DIR, "results", "Names.txt")
+
+with open(file_name_two, mode = "wt", encoding="utf-8") as resul_file:
+    for last_name, first_name, parent in list_data:    
+        string = f"{last_name} {first_name[0]}.{parent[0]}."
+        resul_file.write(string + "\n")
+
+
+
